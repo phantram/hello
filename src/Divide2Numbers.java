4043bdf747b32acf38aeba905730bf06c7c382ef
tram.phan.code <tram.phan.code@gmail.com>
2017-06-17 13:13:37 -0400

    Divide 2 Numbers
    Change-Id: If791bc88df978fc1c5c1d45b905a9f2d64d5913b

---
+++ src/Divide2Numbers.java
@@ -0,0 +1,8 @@
+/**
+ * This program computes division of 2 numbers.
+ */
+public final class Divide2Numbers {
+    public static void main(String[] args) {
+        System.out.println(50 / 3);
+    }
+}
