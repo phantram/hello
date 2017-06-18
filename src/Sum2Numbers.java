132a1fabf3a231f0188ed8b6c53fae9207ec050a
tram.phan.code <tram.phan.code@gmail.com>
2017-06-17 21:31:10 -0400

    Sum of 2 Numbers inputted by user.
    Change-Id: I77a2bbca5d5afe2e14a2d9714c79720b946aa0c2

--- src/Sum2Numbers.java
+++ src/Sum2Numbers.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 /**
  * This program computes sum of 2 numbers.
  */
 public final class Sum2Numbers {
-    public static void main(String[] args) {
-        System.out.println(74 + 36);
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x + y);
     }
 }
